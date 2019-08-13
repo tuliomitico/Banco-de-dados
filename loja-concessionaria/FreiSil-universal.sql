c294f233b2f95f31eecd481a1a8b4530a6d47ca9
Túlio de Freitas Castro <tulioneedforspeed@gmail.com>
2019-08-13 18:35:51 -0300

    Update FreiSil-universal.sql

--- loja-concessionaria/FreiSil-universal.sql
+++ loja-concessionaria/FreiSil-universal.sql
@@ -1,1 +1,33 @@
+DROP SCHEMA IF EXISTS loja1;
 
+CREATE SCHEMA loja1;
+
+CREATE DOMAIN loja1.tipo_cpf AS VARCHAR(13);
+CREATE DOMAIN loja1.tipo_cnpj AS VARCHAR(30);
+
+CREATE TABLE loja1.pessoa (
+    cpf loja1.tipo_cpf NOT NULL,
+    nome VARCHAR(45) NOT NULL,
+    sobrenome VARCHAR(45) NOT NULL,
+    rg VARCHAR(11) NOT NULL,
+    telefone VARCHAR(16) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    data_nascimento DATE NOT NULL,
+    CONSTRAINT pessoa_pkey PRIMARY KEY (cpf)
+);
+
+CREATE TABLE loja1.produto (
+    cod_produto INT NOT NULL,
+    preco_venda REAL NOT NULL CHECK (preco_venda > 0),
+    preco_custo REAL NOT NULL CHECK (preco_custo > 0),
+    data_compra DATE NOT NULL,
+    CONSTRAINT produto_pkey PRIMARY KEY (cod_produto)
+);
+
+CREATE TABLE loja1.fornecedor (
+    cnpj loja1.tipo_cnpj NOT NULL,
+    nome VARCHAR(45) NOT NULL,
+    telefone VARCHAR(16) NOT NULL,
+    endereco VARCHAR(200) NOT NULL,
+    CONSTRAINT fornecedor_pkey PRIMARY KEY (cnpj)
+);
