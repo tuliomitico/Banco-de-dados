59fa212df26f36480d26978864510ad11a4c6284
Túlio de Freitas Castro <tulioneedforspeed@gmail.com>
2019-08-15 13:16:59 -0300

    Update Funcoes interessantes.sql

--- Funcoes interessantes.sql
+++ Funcoes interessantes.sql
@@ -17,3 +17,19 @@ set search_path to casas;
 SELECT SUBSTR(cpf, 1, 3) || '.' || SUBSTR(cpf, 4, 3) || '.' ||
 SUBSTR(cpf, 7, 3) || '-' || SUBSTR(cpf, 10) AS CPF
 FROM natural_person
+
+--Formatar cpf de uma maneira tão dificil quanto
+
+SELECT  
+REGEXP_REPLACE (cpf,  '(\d{3})(\d{3})(\d{3})(\d{2})',  '\1.\2.\3-\4') AS cpf 
+FROM tb_alunos;
+
+--Gerando uma funcao que formata cpf
+
+CREATE OR REPLACE FUNCTION formata_cpf(
+  p_cpf IN VARCHAR2
+) RETURN VARCHAR2
+IS
+BEGIN
+  RETURN regexp_replace(LPAD(p_cpf, 11),'(\d{3})(\d{3})(\d{3})','\1.\2.\3-\4');
+END;
