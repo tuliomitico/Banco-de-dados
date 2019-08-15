c98b6c3930e72ca8467e78512336ea2e37c6e26f
Túlio de Freitas Castro <tulioneedforspeed@gmail.com>
2019-08-15 18:39:39 -0300

    Criar bd1.java
    Aplicação de banco de dados

---
+++ Java/Bd1.java
@@ -0,0 +1,45 @@
+package bd1;
+import java.sql.*;
+
+public class Bd1{
+    public Connection setConnection() throws SQLException{
+    String host = "10.27.159.214:5432";
+    String database = "bd_aula";
+    String url ="jdbc:postgresql://" + host + "/" + database;
+    String user = "aluno1";
+    String password ="aluno1";
+    Connection conexao = DriverManager.getConnection(url,user,password);
+    System.out.println("Criou a conexao");
+    return conexao;
+    }
+  public Bd1() {
+    try{
+    //carregar driver JDBC do postgres
+    Class.forName("org.postgresql.Driver");
+    System.out.println("Carregou");
+    } catch (ClassNotFOundException ex){ex.printStackTrace();}
+  }
+   public void exemploConsultaStatment(Connection conexao) throws
+    SQLException{
+    String sql = "Select cpf from universidade.professor where departamento = 'DCOMP'";
+    Statement comando = conexao.createStatement();
+    System.out.println("Executar consulta: " + sql);
+    ResultSet resultado = comando.executeQuery(sql);
+    while(resultado.next()){
+        String cpf = resultado.getString("cpf");
+        String cpf2 = resultado.getString(1);
+        //String nome3 = resultado.getString(2);
+        System.out.println(cpf);
+        }
+    comando.close();
+    }   
+  
+  public static void main(String[] args) {
+        // TODO code application logic here
+        Bd1 teste = new Bd1();
+        try {teste.setConnection();
+             teste.exemploConsultaStatment();
+        } catch (SQLException e){e.printStackTrace();}
+        
+    }
+}
