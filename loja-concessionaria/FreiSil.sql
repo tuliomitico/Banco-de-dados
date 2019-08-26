15bd53c46034189ca2a7d6c5d2f32b64629c7877
Túlio de Freitas Castro <tulioneedforspeed@gmail.com>
2019-08-26 14:06:02 -0300

    Atualizar FreiSil.sql
    Todos valores inseridos

--- loja-concessionaria/FreiSil.sql
+++ loja-concessionaria/FreiSil.sql
@@ -256,14 +256,6 @@ ALTER TABLE ONLY loja1.venda
 ALTER TABLE ONLY loja1.fidelidade
     ADD CONSTRAINT vendas_nf1 FOREIGN KEY (venda_nf1) REFERENCES loja1.venda(nf);
     
-/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
-
---VENDA
-INSERT INTO loja1.venda (nf, preco_total, data_venda, cliente_cod_cliente, cliente_pessoa_cpf, funcionario_pessoa_cpf) VALUES ()
-
---POSSUI
-INSERT INTO loja1.possui (venda_nf, produto_cod_produto, quantidade) VALUES ()
-
 --PRODUTO
 INSERT INTO loja1.produto (cod_produto, preco_venda, preco_custo, data_compra) VALUES (13465789, 25000, 18000, '2018-10-15');
 INSERT INTO loja1.produto (cod_produto, preco_venda, preco_custo, data_compra) VALUES (13465790, 30000, 22000, '2018-12-02');
@@ -288,6 +280,18 @@ INSERT INTO loja1.produto (cod_produto, preco_venda, preco_custo, data_compra) V
 INSERT INTO loja1.produto (cod_produto, preco_venda, preco_custo, data_compra) VALUES (13465809, 98, 50, '2019-02-21');
 INSERT INTO loja1.produto (cod_produto, preco_venda, preco_custo, data_compra) VALUES (13465810, 98, 50, '2019-02-21');
 INSERT INTO loja1.produto (cod_produto, preco_venda, preco_custo, data_compra) VALUES (13465811, 98, 50, '2019-02-21');
+
+--FORNECEDOR
+INSERT INTO loja1.fornecedor (cnpj, nome, telefone, endereco) VALUES ('32.777.123/9821-22', 'Ford', '(41)99887-9820', 'Rua Carmelina, 42, Santos Drummont');
+INSERT INTO loja1.fornecedor (cnpj, nome, telefone, endereco) VALUES ('32.777.123/9821-23', 'Toyota', '(41)99887-2333', 'Rua Josefa, 22, São Paulo');
+INSERT INTO loja1.fornecedor (cnpj, nome, telefone, endereco) VALUES ('32.777.123/9821-24', 'Fiat', '(41)99887-1240', 'Rua Carmelina, 432, Santos Drummont');
+INSERT INTO loja1.fornecedor (cnpj, nome, telefone, endereco) VALUES ('32.777.123/9821-25', 'Volkswagen', '(41)99143-4420', 'Rua Carmelina, 142, Santos Drummont');
+INSERT INTO loja1.fornecedor (cnpj, nome, telefone, endereco) VALUES ('32.777.123/9821-26', 'Peugeot', '(41)99187-9812', 'Rua Carmelina, 453, Santos Drummont');
+INSERT INTO loja1.fornecedor (cnpj, nome, telefone, endereco) VALUES ('32.777.123/9821-27', 'Chevrolet', '(41)99187-0924', 'Rua Carmelina, 521, Santos Drummont');
+INSERT INTO loja1.fornecedor (cnpj, nome, telefone, endereco) VALUES ('32.777.123/9821-28', 'Renault', '(41)99287-9115', 'Rua Carmelina, 532, Santos Drummont');
+INSERT INTO loja1.fornecedor (cnpj, nome, telefone, endereco) VALUES ('32.777.123/9821-29', 'Audi', '(41)91264-8629', 'Rua Carmelina, 222, Santos Drummont');
+INSERT INTO loja1.fornecedor (cnpj, nome, telefone, endereco) VALUES ('32.777.123/9821-30', 'Mitsubishi', '(41)99876-9822', 'Rua Carmelina, 111, Santos Drummont');
+INSERT INTO loja1.fornecedor (cnpj, nome, telefone, endereco) VALUES ('32.777.123/9821-31', 'Hyundai', '(41)99198-9848', 'Rua Carmelina, 908, Santos Drummont');
 
 --CARRO
 INSERT INTO loja1.carro (num_chassi, modelo, cor, marca, cod_veicular, produto_cod_produto1, ano) VALUES (23465789, 'Onyx', 'Prata', 'Chevrolet', 123476, 13465789, 2015);
@@ -317,23 +321,40 @@ INSERT INTO loja1."peça" (cod_peca, tipo, nome, descricao, produto_cod_produto2
 INSERT INTO loja1."peça" (cod_peca, tipo, nome, descricao, produto_cod_produto2) VALUES(19, 'Acessório Interior', 'Maçaneta interna', 'Maçaneta interna para Onyx', 13465811);
 
 --ABASTECE
-INSERT INTO loja1.abastece (lote, abastececol, produto_cod_produto, fornecedor_cnpj, data_entrega) VALUES()
-
---FORNECEDOR
-INSERT INTO loja1.fornecedor (cnpj, nome, telefone, endereco) VALUES ()
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(1, '2019-01-12', 13465789, '32.777.123/9821-27');
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(2, '2019-01-15', 13465790, '32.777.123/9821-31');
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(3, '2019-01-02', 13465791, '32.777.123/9821-22');
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(4, '2019-01-01', 13465792, '32.777.123/9821-27');
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(5, '2018-09-02', 13465793, '32.777.123/9821-27');
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(6, '2019-09-02', 13465794, '32.777.123/9821-27');
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(7, '2018-08-01', 13465795, '32.777.123/9821-27');
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(8, '2019-10-01', 13465796, '32.777.123/9821-27');
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(9, '2019-04-01', 13465797, '32.777.123/9821-22');
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(10, '2019-03-01', 13465798, '32.777.123/9821-22');
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(11, '2019-03-01', 13465799, '32.777.123/9821-22');
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(12, '2019-03-01', 13465800, '32.777.123/9821-22');
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(13, '2019-01-01', 13465801, '32.777.123/9821-22');
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(14, '2017-11-01', 13465802, '32.777.123/9821-22');
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(15, '2017-11-01', 13465803, '32.777.123/9821-27');
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(16, '2017-11-01', 13465804, '32.777.123/9821-27');
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(17, '2017-12-01', 13465805, '32.777.123/9821-27');
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(18, '2018-12-01', 13465806, '32.777.123/9821-27');
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(19, '2018-12-01', 13465807, '32.777.123/9821-27');
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(20, '2019-07-01', 13465808, '32.777.123/9821-27');
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(21, '2019-07-01', 13465809, '32.777.123/9821-27');
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(22, '2019-07-01', 13465810, '32.777.123/9821-27');
+INSERT INTO loja1.abastece (lote, data_entrega, produto_cod_produto3, fornecedor_cnpj) VALUES(23, '2019-06-01', 13465811, '32.777.123/9821-27');
 
 --PESSOA
 INSERT INTO loja1.pessoa (cpf,nome,sobrenome,rg,telefone,email,data_nascimento) VALUES ('11122233344','Caio','Pinheiro Souza','11223344556','3213-6540','caio@gmail.com','1981-03-28');
 INSERT INTO loja1.pessoa (cpf,nome,sobrenome,rg,telefone,email,data_nascimento) VALUES ('11122233345','Francine','Sterling','11223344557','3220-7589','francine@live.com','1993-08-17');
 INSERT INTO loja1.pessoa (cpf,nome,sobrenome,rg,telefone,email,data_nascimento) VALUES ('11122233346','Diego','Rubens Silva','11223344558','99220-8975','diego@bol.com','1979-06-27');
 INSERT INTO loja1.pessoa (cpf,nome,sobrenome,rg,telefone,email,data_nascimento) VALUES ('11122233347','Kevin','Costa Pinto','11223344559','98890-3344','kevin@outlook.com','2000-04-01');
-
 INSERT INTO loja1.pessoa (cpf,nome,sobrenome,rg,telefone,email,data_nascimento) VALUES ('11122233348','Caio','Santos Teixeira','11223344560','98890-3744','kevin@outlook.com','1985-04-01');
 INSERT INTO loja1.pessoa (cpf,nome,sobrenome,rg,telefone,email,data_nascimento) VALUES ('11122233349','Ângela','Rocha Castro','11223344561','98890-3844','angela@gmail.com','1977-05-01');
 INSERT INTO loja1.pessoa (cpf,nome,sobrenome,rg,telefone,email,data_nascimento) VALUES ('11122233350','Wilda','Silva Rocha','11223344562','98890-3944','wilda@gmail.com','1990-06-01');
 INSERT INTO loja1.pessoa (cpf,nome,sobrenome,rg,telefone,email,data_nascimento) VALUES ('11122233351','Eliseo','Santos Neto','11223344563','98890-4044','eliseo@outlook.com','1995-01-01');
 INSERT INTO loja1.pessoa (cpf,nome,sobrenome,rg,telefone,email,data_nascimento) VALUES ('11122233352','Max','Dantas Bittencourt','11223344564','98991-5444','max@live.com','1996-09-01');
-
 INSERT INTO loja1.pessoa (cpf,nome,sobrenome,rg,telefone,email,data_nascimento) VALUES ('11122233353','Sandra','Queiroz Rocha','11223344565','98950-1354','sandra@yahoo.com','1993-04-01');
 INSERT INTO loja1.pessoa (cpf,nome,sobrenome,rg,telefone,email,data_nascimento) VALUES ('11122233354','Ashley','Sandra Teixeira','11223344566','98861-4368','ashley@yahoo.com','1989-05-01');
 INSERT INTO loja1.pessoa (cpf,nome,sobrenome,rg,telefone,email,data_nascimento) VALUES ('11122233355','Elias',' Pedro Santana Júnior','11223344567','99146-4503','elias@gmail.com','1967-11-01');
@@ -341,15 +362,15 @@ INSERT INTO loja1.pessoa (cpf,nome,sobrenome,rg,telefone,email,data_nascimento) 
 INSERT INTO loja1.pessoa (cpf,nome,sobrenome,rg,telefone,email,data_nascimento) VALUES ('11122233357','Sandra','Silva Matos','11223344569','99148-3755','sandrasm@gmail.com','1994-10-01');
 
 --CLIENTE
-INSERT INTO loja1.cliente (cod_cliente, pts_fidelidade,pessoa_cpf1) VALUES (1, 0,'11122233344');
-INSERT INTO loja1.cliente (cod_cliente, pts_fidelidade,pessoa_cpf1) VALUES (2, 260,'11122233345');
-INSERT INTO loja1.cliente (cod_cliente, pts_fidelidade,pessoa_cpf1) VALUES (3, 150,'11122233346');
-INSERT INTO loja1.cliente (cod_cliente, pts_fidelidade,pessoa_cpf1) VALUES (4, 50,'11122233347');
-INSERT INTO loja1.cliente (cod_cliente, pts_fidelidade,pessoa_cpf1) VALUES (5, 700,'11122233348');
-INSERT INTO loja1.cliente (cod_cliente, pts_fidelidade,pessoa_cpf1) VALUES (6, 130,'11122233349');
-INSERT INTO loja1.cliente (cod_cliente, pts_fidelidade,pessoa_cpf1) VALUES (7, 500,'11122233350');
-INSERT INTO loja1.cliente (cod_cliente, pts_fidelidade,pessoa_cpf1) VALUES (8, 1300,'11122233351');
-INSERT INTO loja1.cliente (cod_cliente, pts_fidelidade,pessoa_cpf1) VALUES (9, 220,'11122233352');
+INSERT INTO loja1.cliente (cod_cliente, pts_fidelidade, pessoa_cpf1) VALUES (1, 0, '11122233344');
+INSERT INTO loja1.cliente (cod_cliente, pts_fidelidade, pessoa_cpf1) VALUES (2, 260, '11122233345');
+INSERT INTO loja1.cliente (cod_cliente, pts_fidelidade, pessoa_cpf1) VALUES (3, 150, '11122233346');
+INSERT INTO loja1.cliente (cod_cliente, pts_fidelidade, pessoa_cpf1) VALUES (4, 50, '11122233347');
+INSERT INTO loja1.cliente (cod_cliente, pts_fidelidade, pessoa_cpf1) VALUES (5, 700, '11122233348');
+INSERT INTO loja1.cliente (cod_cliente, pts_fidelidade, pessoa_cpf1) VALUES (6, 130, '11122233349');
+INSERT INTO loja1.cliente (cod_cliente, pts_fidelidade, pessoa_cpf1) VALUES (7, 500, '11122233350');
+INSERT INTO loja1.cliente (cod_cliente, pts_fidelidade, pessoa_cpf1) VALUES (8, 1300, '11122233351');
+INSERT INTO loja1.cliente (cod_cliente, pts_fidelidade, pessoa_cpf1) VALUES (9, 220, '11122233352');
 
 --FUNCIONARIO
 INSERT INTO loja1.funcionario (curriculo, senha, salario, pessoa_cpf) VALUES('C://Arquivos de Programas/Curriculos/', '0000', 1200, '11122233353');
@@ -357,6 +378,53 @@ INSERT INTO loja1.funcionario (curriculo, senha, salario, pessoa_cpf) VALUES('C:
 INSERT INTO loja1.funcionario (curriculo, senha, salario, pessoa_cpf) VALUES('C://Arquivos de Programas/Curriculos/', '0000', 4000, '11122233355');
 INSERT INTO loja1.funcionario (curriculo, senha, salario, pessoa_cpf) VALUES('C://Arquivos de Programas/Curriculos/', '0000', 11000, '11122233356');
 INSERT INTO loja1.funcionario (curriculo, senha, salario, pessoa_cpf) VALUES('C://Arquivos de Programas/Curriculos/', '0000', 3500, '11122233357');
+
+--VENDA
+INSERT INTO loja1.venda (nf, preco_total, data_venda, cliente_cod_cliente1, cliente_pessoa1_cpf, funcionario_pessoa1_cpf) VALUES (1, 120, '2019-05-01', 2, '11122233345', '11122233353');
+INSERT INTO loja1.venda (nf, preco_total, data_venda, cliente_cod_cliente1, cliente_pessoa1_cpf, funcionario_pessoa1_cpf) VALUES (2, 80, '2019-05-02', 3, '11122233346', '11122233354');
+INSERT INTO loja1.venda (nf, preco_total, data_venda, cliente_cod_cliente1, cliente_pessoa1_cpf, funcionario_pessoa1_cpf) VALUES (3, 200, '2019-05-02', 4, '11122233347', '11122233354');
+INSERT INTO loja1.venda (nf, preco_total, data_venda, cliente_cod_cliente1, cliente_pessoa1_cpf, funcionario_pessoa1_cpf) VALUES (4, 320, '2019-05-02', 5, '11122233348', '11122233353');
+INSERT INTO loja1.venda (nf, preco_total, data_venda, cliente_cod_cliente1, cliente_pessoa1_cpf, funcionario_pessoa1_cpf) VALUES (5, 410, '2019-05-01', 6, '11122233349', '11122233355');
+INSERT INTO loja1.venda (nf, preco_total, data_venda, cliente_cod_cliente1, cliente_pessoa1_cpf, funcionario_pessoa1_cpf) VALUES (6, 80, '2019-06-01', 7, '11122233350', '11122233356');
+INSERT INTO loja1.venda (nf, preco_total, data_venda, cliente_cod_cliente1, cliente_pessoa1_cpf, funcionario_pessoa1_cpf) VALUES (7, 43000, '2019-06-01', 8, '11122233351', '11122233356');
+INSERT INTO loja1.venda (nf, preco_total, data_venda, cliente_cod_cliente1, cliente_pessoa1_cpf, funcionario_pessoa1_cpf) VALUES (8, 400, '2019-07-12', 9, '11122233352', '11122233357');
+INSERT INTO loja1.venda (nf, preco_total, data_venda, cliente_cod_cliente1, cliente_pessoa1_cpf, funcionario_pessoa1_cpf) VALUES (9, 280, '2019-07-20', 3, '11122233346', '11122233357');
+INSERT INTO loja1.venda (nf, preco_total, data_venda, cliente_cod_cliente1, cliente_pessoa1_cpf, funcionario_pessoa1_cpf) VALUES (10, 30000, '2019-06-11', 9, '11122233352', '11122233354');
+INSERT INTO loja1.venda (nf, preco_total, data_venda, cliente_cod_cliente1, cliente_pessoa1_cpf, funcionario_pessoa1_cpf) VALUES (11, 95, '2019-08-24', 3, '11122233346', '11122233355');
+
+--FIDELIDADE
+INSERT INTO loja1.fidelidade (cod_fidelidade, qtd_ponto, validade, venda_nf1, cliente_cod_cliente2, cliente_pessoa2_cpf) VALUES(1, 12, '2022-01-01', 1, 2, '11122233345');
+INSERT INTO loja1.fidelidade (cod_fidelidade, qtd_ponto, validade, venda_nf1, cliente_cod_cliente2, cliente_pessoa2_cpf) VALUES(2, 8, '2022-01-01', 2, 3, '11122233346');
+INSERT INTO loja1.fidelidade (cod_fidelidade, qtd_ponto, validade, venda_nf1, cliente_cod_cliente2, cliente_pessoa2_cpf) VALUES(3, 20, '2022-01-01', 3, 4, '11122233347');
+INSERT INTO loja1.fidelidade (cod_fidelidade, qtd_ponto, validade, venda_nf1, cliente_cod_cliente2, cliente_pessoa2_cpf) VALUES(4, 32, '2022-01-01', 4, 5, '11122233348');
+INSERT INTO loja1.fidelidade (cod_fidelidade, qtd_ponto, validade, venda_nf1, cliente_cod_cliente2, cliente_pessoa2_cpf) VALUES(5, 41, '2022-01-01', 5, 6, '11122233349');
+INSERT INTO loja1.fidelidade (cod_fidelidade, qtd_ponto, validade, venda_nf1, cliente_cod_cliente2, cliente_pessoa2_cpf) VALUES(6, 8, '2022-01-01', 6, 7, '11122233350');
+INSERT INTO loja1.fidelidade (cod_fidelidade, qtd_ponto, validade, venda_nf1, cliente_cod_cliente2, cliente_pessoa2_cpf) VALUES(7, 4300, '2022-01-01', 7, 8, '11122233351');
+INSERT INTO loja1.fidelidade (cod_fidelidade, qtd_ponto, validade, venda_nf1, cliente_cod_cliente2, cliente_pessoa2_cpf) VALUES(8, 40, '2022-01-01', 8, 9, '11122233352');
+INSERT INTO loja1.fidelidade (cod_fidelidade, qtd_ponto, validade, venda_nf1, cliente_cod_cliente2, cliente_pessoa2_cpf) VALUES(9, 28, '2022-01-01', 9, 3, '11122233346');
+INSERT INTO loja1.fidelidade (cod_fidelidade, qtd_ponto, validade, venda_nf1, cliente_cod_cliente2, cliente_pessoa2_cpf) VALUES(10, 3000, '2022-01-01', 10, 9, '11122233352');
+INSERT INTO loja1.fidelidade (cod_fidelidade, qtd_ponto, validade, venda_nf1, cliente_cod_cliente2, cliente_pessoa2_cpf) VALUES(11, 10, '2022-01-01', 11, 3, '11122233346');
+
+--POSSUI
+INSERT INTO loja1.possui (venda_nf, produto_cod_produto, quantidade) VALUES (1, 13465793, 1);
+INSERT INTO loja1.possui (venda_nf, produto_cod_produto, quantidade) VALUES (2, 13465795, 1);
+INSERT INTO loja1.possui (venda_nf, produto_cod_produto, quantidade) VALUES (3, 13465793, 1);
+INSERT INTO loja1.possui (venda_nf, produto_cod_produto, quantidade) VALUES (3, 13465795, 1);
+INSERT INTO loja1.possui (venda_nf, produto_cod_produto, quantidade) VALUES (4, 13465793, 1);
+INSERT INTO loja1.possui (venda_nf, produto_cod_produto, quantidade) VALUES (4, 13465797, 4);
+INSERT INTO loja1.possui (venda_nf, produto_cod_produto, quantidade) VALUES (5, 13465794, 1);
+INSERT INTO loja1.possui (venda_nf, produto_cod_produto, quantidade) VALUES (5, 13465798, 2);
+INSERT INTO loja1.possui (venda_nf, produto_cod_produto, quantidade) VALUES (5, 13465799, 4);
+INSERT INTO loja1.possui (venda_nf, produto_cod_produto, quantidade) VALUES (6, 13465795, 1);
+INSERT INTO loja1.possui (venda_nf, produto_cod_produto, quantidade) VALUES (7, 13465791, 1);
+INSERT INTO loja1.possui (venda_nf, produto_cod_produto, quantidade) VALUES (8, 13465798, 4);
+INSERT INTO loja1.possui (venda_nf, produto_cod_produto, quantidade) VALUES (8, 13465799, 4);
+INSERT INTO loja1.possui (venda_nf, produto_cod_produto, quantidade) VALUES (9, 13465795, 1);
+INSERT INTO loja1.possui (venda_nf, produto_cod_produto, quantidade) VALUES (9, 13465796, 2);
+INSERT INTO loja1.possui (venda_nf, produto_cod_produto, quantidade) VALUES (9, 13465799, 2);
+INSERT INTO loja1.possui (venda_nf, produto_cod_produto, quantidade) VALUES (10, 13465790, 1);
+INSERT INTO loja1.possui (venda_nf, produto_cod_produto, quantidade) VALUES (11, 13465806, 1);
+
 
 --ADMINISTRA
 INSERT INTO loja1.administra (funcionario_pessoa_cpf, cliente_cod_cliente, cliente_pessoa_cpf) VALUES ('11122233353', 1, '11122233344');
@@ -368,9 +436,6 @@ INSERT INTO loja1.administra (funcionario_pessoa_cpf, cliente_cod_cliente, clien
 INSERT INTO loja1.administra (funcionario_pessoa_cpf, cliente_cod_cliente, cliente_pessoa_cpf) VALUES ('11122233356', 7, '11122233350');
 INSERT INTO loja1.administra (funcionario_pessoa_cpf, cliente_cod_cliente, cliente_pessoa_cpf) VALUES ('11122233357', 8, '11122233351');
 INSERT INTO loja1.administra (funcionario_pessoa_cpf, cliente_cod_cliente, cliente_pessoa_cpf) VALUES ('11122233354', 9, '11122233352');
-
---FIDELIDADE
-INSERT INTO loja1.fidelidade (cod_fidelidade, qtd_ponto, validade, cliente_cod_cliente, cliente_pessoa_cpf, venda_nf) VALUES()
 
 --ENDERECO
 INSERT INTO loja1."endereço" (cep, logradouro, bairro, cidade, estado) VALUES ('49025-025', 'Molestie Road', 'Shaine Q. Chandler', 'Aracaju', 'SE');
@@ -403,4 +468,5 @@ INSERT INTO loja1.mora (numero, complemento, pessoa_cpf, "endereço_cep") VALUES
 INSERT INTO loja1.mora (numero, complemento, pessoa_cpf, "endereço_cep") VALUES (15, '', '11122233355', '49025-136');
 INSERT INTO loja1.mora (numero, complemento, pessoa_cpf, "endereço_cep") VALUES (45, '', '11122233356', '49025-237');
 INSERT INTO loja1.mora (numero, complemento, pessoa_cpf, "endereço_cep") VALUES (354, '', '11122233357', '49025-938');
+
 
