fe60e7c42707c0fc89c1c00916f1a5fe69e9c1ee
Túlio de Freitas Castro <tulioneedforspeed@gmail.com>
2019-08-27 19:38:18 -0300

    Update trabalho parte 1.sql

--- loja-concessionaria/trabalho parte 1.sql
+++ loja-concessionaria/trabalho parte 1.sql
@@ -8,9 +8,11 @@ select * from loja1."endereço" where bairro='Jardins'
 
 --Listar o marca, modelo, preço de venda dos carros com preço de venda acima de 29000 reais
 
-select * from loja1.carro;
-select * from loja1.produto;
 select ca.marca,ca.modelo,pro.preco_venda 
-from loja1.carro as ca join loja1.produto as pro on (cod_produto=produto_cod_produto1)
+from loja1.carro as ca join loja1.produto as pro on (ca.cod_produto=pro.cod_produto1)
 where pro.preco_venda > 29000
 
+--Listar cep,numero,cpf,nome das pessoas
+
+select mo.cep,mo.numero,mo.cpf,pe.nome 
+from mora as mo join endereço as en using (cep) join pessoa as pe on (mo.cpf = pe.cpf);
